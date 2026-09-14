4733d707694c03536cc84d60ab62e5b48e2c1675
Seedora <kanato3419@gmail.com>
2026-09-14 22:55:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const mobileMenu = document.querySelector(".mobile-menu");
 menuButton.addEventListener("click", function () {
     mobileMenu.classList.toggle("active");
 });
+/* ==============================
+   Opening Animation
+============================== */
+
+const opening = document.querySelector(".opening");
+
+if (opening) {
+
+    setTimeout(() => {
+        opening.classList.add("opening-hidden");
+    }, 4900);
+
+}
